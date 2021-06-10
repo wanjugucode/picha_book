0c4dcd9b53a64b2e849af130a7c312017e523cfd
wanjugucode <karobiaanastasia.com>
2021-06-10 21:57:30 +0300

    code

--- app/src/main/java/com/example/beautyapp/facefive.kt
+++ app/src/main/java/com/example/beautyapp/facefive.kt
@@ -1,11 +1,20 @@
 package com.example.beautyapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class facefive : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_facefive)
+
+        var btnPrevious4=findViewById<Button>(R.id.btnPrevious5)
+        btnPrevious4.setOnClickListener{
+            var intent= Intent(baseContext,facefour::class.java)
+            startActivity(intent)
+
+        }
     }
 }
--- app/src/main/java/com/example/beautyapp/facefour.kt
+++ app/src/main/java/com/example/beautyapp/facefour.kt
@@ -9,16 +9,14 @@ class facefour : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_facefour)
-
-        setContentView(R.layout.activity_facethree)
-        var btnPrevious3=findViewById<Button>(R.id.btnPrevious2)
-        var btnNext4=findViewById<Button>(R.id.btnNext3)
-        btnPrevious3.setOnClickListener{
+        var btnPrevious2=findViewById<Button>(R.id.btnPrevious3)
+        var btnNext3=findViewById<Button>(R.id.btnNext4)
+        btnPrevious2.setOnClickListener{
             var intent= Intent(baseContext,facethree::class.java)
             startActivity(intent)
 
         }
-        btnNext4.setOnClickListener {
+        btnNext3.setOnClickListener {
             var intent= Intent(baseContext,facefive::class.java)
             startActivity(intent)
 
