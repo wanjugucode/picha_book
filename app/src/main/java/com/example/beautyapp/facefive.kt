139c0406280fd51fc530cb1eb44d9a147c73f82d
wanjugucode <karobiaanastasia.com>
2021-06-10 21:52:20 +0300

    code

--- app/src/main/java/com/example/beautyapp/facefive.kt
+++ app/src/main/java/com/example/beautyapp/facefive.kt
@@ -1,21 +1,11 @@
 package com.example.beautyapp
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 
 class facefive : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_facefive)
-        setContentView(R.layout.activity_facethree)
-        var btnPrevious4=findViewById<Button>(R.id.btnPrevious4)
-        btnPrevious4.setOnClickListener{
-            var intent= Intent(baseContext,MainActivity::class.java)
-            startActivity(intent)
-
-        }
-
     }
 }
